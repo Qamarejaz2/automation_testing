e61d3cfb4d27c4e1502f603f8ee5be111ea40e81
Qamar Ejaz <134260492+Qamarejaz2@users.noreply.github.com>
2023-10-06 12:55:28 +0500

    Update validtoken.py

--- validtoken.py
+++ validtoken.py
@@ -7,21 +7,21 @@ app = Flask(__name__)
 def requires_auth_token(f):
     @wraps(f)
     def decorated(*args, **kwargs):
-        # Get the Authorization header from the request
+       
         auth_header = request.headers.get('Authorization')
         
-        # Check if the Authorization header is present
+     
         if not auth_header:
             return jsonify({'message': 'Authorization token is missing'}), 401
 
-        # Extract the token from the Authorization header
+       
         auth_token = auth_header.split(' ')[1] if len(auth_header.split(' ')) > 1 else None
 
-        # Check if the token is valid
+        
         if not auth_token or auth_token != 'YOUR_AUTH_TOKEN':
             return jsonify({'message': 'Invalid authorization token'}), 401
 
-        # Call the decorated function if the token is valid
+        
         return f(*args, **kwargs)
 
     return decorated
