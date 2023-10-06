a56ce4e5192a1f6d63bc3f0d32ba9cbf8cf5b50b
Qamar Ejaz <134260492+Qamarejaz2@users.noreply.github.com>
2023-10-06 12:57:56 +0500

    Update getprimarykey.py

--- getprimarykey.py
+++ getprimarykey.py
@@ -16,31 +16,28 @@ def get_primary_key():
                 database='db'
             )
 
-            # Create a cursor object to execute SQL queries
+           
             cursor = conn.cursor()
 
-            # Execute a SELECT query to retrieve the primary key
-            cursor.execute("SELECT * from table")  # Replace 'your_table' with your actual table name
-
-            # Fetch the primary key value
+          
+            cursor.execute("SELECT * from table")  
+          
             primary_key = cursor.fetchone()[0]
 
-            # Close the database connection
             conn.close()
 
-            # Return the primary key as JSON response
+           
             return jsonify({'primary_key': primary_key})
 
         elif request.method == 'POST':
-            # Handle the POST request if needed
-            # Add your POST request handling code here
+           
             return jsonify({'message': 'POST request handled'})
 
     except Exception as e:
-        # Log the error for debugging purposes
+        
         app.logger.error(str(e))
         print (e)
-        # Return a JSON response with the error message
+        
         return jsonify({'error': 'Internal Server Error'}), 500
     
 
