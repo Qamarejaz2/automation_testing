ac95d4e610af6cf7c929081f09812b40526b44e1
Qamar Ejaz <134260492+Qamarejaz2@users.noreply.github.com>
2023-10-06 12:56:46 +0500

    Update jwtlog_token.py

--- jwtlog_token.py
+++ jwtlog_token.py
@@ -10,25 +10,25 @@ import jwt
 def requires_auth_token(f):
     @wraps(f)
     def decorated(*args, **kwargs):
-        # Get the Authorization header from the request
+       
         auth_header = request.headers.get('Authorization')
 
-        # Check if the Authorization header is present
+       
         if not auth_header:
             return jsonify({'message': 'Authorization token is missing'}), 401
 
-        # Extract the token from the Authorization header
+    
         auth_token = auth_header.split(' ')[1] if len(auth_header.split(' ')) > 1 else None
 
-        # Check if the token is valid
+        
         try:
-            # Verify and decode the token
+            
             decoded_token = jwt.decode(auth_token, app.config['SECRET_KEY'], algorithms=['HS256'])
             request.jwt_data = decoded_token  # Store the decoded token data for later use
         except jwt.exceptions.InvalidTokenError:
             return jsonify({'message': 'Invalid authorization token'}), 401
 
-        # Call the decorated function if the token is valid
+     
         return f(*args, **kwargs)
 
     return decorated
@@ -39,10 +39,7 @@ def login():
     username = request.json.get('username')
     password = request.json.get('password')
 
-    # Perform authentication logic here
-    # You can validate the username and password against a database or any other authentication mechanism
-
-    # Example authentication check
+   
     if username == 'name' and password == 'pwd':
         # Generate JWT token
         token = jwt.encode({'username': username}, app.config['SECRET_KEY'], algorithm='HS256')
